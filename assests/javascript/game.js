aabd90fa1e9948fa7db0560408fe553844705839
Paul Thomas <traitorpt@gmail.com>
2019-08-28 19:43:04 -0700

    added game loop to javascript and improved css layout by changing background image and adding shadow, outline and google fonts to the text.

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -2,41 +2,41 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var guessesMade = [];
+var playGame = confirm('Do you want to play The Psychic Game');
+
+if (playGame) {
+    document.onkeyup = function (event) {
+        var userGuess = event.key;
+        guessesMade.push(userGuess);
+        var randomLetter = ('abcdefghijklmnopqrstuvwxyz').split('')[(Math.floor(Math.random() * 26))];
 
 
 
-document.onkeyup = function (event)
-{
-var userGuess = event.key;
-guessesMade.push(userGuess);
-var randomLetter = ('abcdefghijklmnopqrstuvwxyz').split('')[(Math.floor(Math.random() * 26 ))];
+        var reset = function () {
+            guessesLeft = 10;
+            guessesMade = [];
+        }
 
+        if (userGuess === randomLetter) {
+            alert("You win!  The letter was " + randomLetter);
+            wins++;
+            playGame = confirm('Play Again?');
+            reset();
+        } else {
+            guessesLeft--;
+        }
 
+        if (guessesLeft === 0) {
+            alert("You lost!  The letter was " + randomLetter)
+            losses++;
+            playGame = confirm('Play Again?')
+            reset();
+        }
 
-var reset = function () {
-    guessesLeft = 10;
-    guessesMade = [];
+        var html = "<h1>Guess the letter?</h1>" +
+        "<p>Guesses Made:  " + guessesMade + "</p>" + "<p>Guesses Left:  " + guessesLeft + "</p>" + "<p>Wins:  " + wins + "</p>" + "<p>Losses:  " + losses + "</p>";
+        document.getElementById('gameInfo').innerHTML = html;
+
+    }
 }
 
-if (userGuess === randomLetter){
-    alert("You win!  The letter was " + randomLetter);
-    wins++;
-    guessesLeft = 10;
-    reset();
-} else{
-    guessesLeft--;
-}
-
-if (guessesLeft === 0){
-    alert("You lost!  The letter was " + randomLetter)
-    losses++;
-    guessesLeft = 10;
-    reset();
-}
-
-var html = "<h1>The Psychic Game</h1>" +
-"<p>Wins: "+ wins + "</p><p>Losses: " + losses + "</p>" + "<p>Guesses Left: " + guessesLeft + "</p>" + "<p>Guesses Made : " + guessesMade + "</p>";
-
-document.getElementById('gameInfo').innerHTML = html;
-
-}
