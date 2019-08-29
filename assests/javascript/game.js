aa2244875ba80aaddcc619e720efcf27e68dfa5b
Paul Thomas <traitorpt@gmail.com>
2019-08-29 08:19:20 -0700

    added color changing to backgournd of game play.  added alert to let a player know if they repeat a guess.  i removed the commas from the displayed guessed letters array with .join()

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -6,37 +6,42 @@ var playGame = confirm('Do you want to play The Psychic Game');
 
 if (playGame) {
     document.onkeyup = function (event) {
-        var userGuess = event.key;
-        guessesMade.push(userGuess);
-        var randomLetter = ('abcdefghijklmnopqrstuvwxyz').split('')[(Math.floor(Math.random() * 26))];
-
-
-
-        var reset = function () {
-            guessesLeft = 10;
-            guessesMade = [];
+        for (var i = 0; i < guessesMade.length; i++){
+        if (event.key === guessesMade[i]) {
+            guessesLeft++;
+            alert('You already guessed that letter')
+            }
         }
+        
+            var userGuess = event.key;
+            guessesMade.push(userGuess);
+            var randomLetter = ('abcdefghijklmnopqrstuvwxyz').split('')[(Math.floor(Math.random() * 26))];
+    
+            var reset = function () {
+                guessesLeft = 10;
+                guessesMade = [];
+            }
 
-        if (userGuess === randomLetter) {
-            alert("You win!  The letter was " + randomLetter);
-            wins++;
-            playGame = confirm('Play Again?');
-            reset();
-        } else {
-            guessesLeft--;
+            if (userGuess === randomLetter) {
+                alert("You win!  The letter was " + randomLetter);
+                wins++;
+                playGame = confirm('Play Again?');
+                reset();
+            } else {
+                guessesLeft--;
+            }
+
+            if (guessesLeft === 0) {
+                alert("You lost!  The letter was " + randomLetter)
+                losses++;
+                playGame = confirm('Play Again?')
+                reset();
+            }
+
+            var html = "<h1>Guess the letter?</h1>" +
+                "<p>Guesses Made:  </p>" + guessesMade.join(" ") + "<p>Guesses Left:  " + guessesLeft + "</p>" + "<p>Wins:  " + wins + "</p>" + "<p>Losses:  " + losses + "</p>";
+            document.getElementById('gameInfo').innerHTML = html;
         }
-
-        if (guessesLeft === 0) {
-            alert("You lost!  The letter was " + randomLetter)
-            losses++;
-            playGame = confirm('Play Again?')
-            reset();
-        }
-
-        var html = "<h1>Guess the letter?</h1>" +
-        "<p>Guesses Made:  " + guessesMade + "</p>" + "<p>Guesses Left:  " + guessesLeft + "</p>" + "<p>Wins:  " + wins + "</p>" + "<p>Losses:  " + losses + "</p>";
-        document.getElementById('gameInfo').innerHTML = html;
-
     }
-}
+    
 
